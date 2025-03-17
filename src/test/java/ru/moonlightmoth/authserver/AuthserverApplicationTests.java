08969f3d738dc9d0011da210c257441a27503ac9
moonlightmoth <ghjpa@inbox.ru>
2025-03-17 16:40:27 +0300

    init

---
+++ src/test/java/ru/moonlightmoth/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moonlightmoth.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
